03b06bff854e6075deaf09a2485855281eb0cd83
Gustavo Rago <gustavorago@gmail.com>
2014-06-03 17:44:29 -0400

    ADD

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Pasapalabra server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
